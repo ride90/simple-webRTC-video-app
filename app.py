04f04982d80720fdddef28ddbd77a0699628c80c
OlegPshenichniy <olegpshenichniy@gmail.com>
2015-01-31 23:59:06 +0200

    App sources. Update .gitignore.

---
+++ app.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+import os
+import uuid
+
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+from tornado.websocket import WebSocketHandler
+
+import settings
+
+
+# logger
+logger = logging.getLogger('webrtc_server')
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler('webrtc_server.log')
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s || %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+resolve = lambda *dirname: os.path.abspath(os.path.join(settings.ROOT_PATH, *dirname))
+
+GLOBAL_NODES = {}
+
+
+class IndexHandler(RequestHandler):
+    def get(self):
+        node = str(uuid.uuid4().get_hex().upper()[0:6])
+        self.redirect('/node/' + node)
+
+
+class NodeHandler(RequestHandler):
+    def get(self, slug):
+        self.render('node.html', nodes=())
+
+
+class Node(object):
+    def __init__(self, name, clients=[]):
+        self.name = name
+        self.clients = clients
+
+    def __repr__(self):
+        return "Node '{0}'".format(self.name)
+
+
+class WebSocket(WebSocketHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(WebSocket, self).__init__(*args, **kwargs)
+        self.current_node = None
+
+    def open(self, slug):
+        logger.info('WebSocket opening "{0}" from %s'.format(slug), self.request.remote_ip)
+
+        if slug not in GLOBAL_NODES:
+            GLOBAL_NODES[slug] = Node(slug, [self])
+        else:
+            GLOBAL_NODES[slug].clients.append(self)
+
+        self.current_node = GLOBAL_NODES[slug]
+
+        if len(self.current_node.clients) == 1:
+            self.write_message('owner')
+        elif len(self.current_node.clients) > 2:
+            self.write_message('magic_overload')
+        else:
+            self.write_message('guest')
+
+        logger.info('WebSocket opened "{0}" from %s'.format(slug), self.request.remote_ip)
+
+    def on_message(self, message):
+        logger.info('Received message from %s: %s', self.request.remote_ip, message)
+
+        for client in self.current_node.clients:
+            if client is self:
+                continue
+            client.write_message(message)
+
+    def on_close(self):
+        logger.info('WebSocket connection closed.')
+        self.current_node.clients.remove(self)
+
+
+def main():
+    app_settings = {'template_path': resolve('templates'),
+                    'static_path': resolve('static'),
+                    'debug': True}
+
+    application = Application([(r'/', IndexHandler),
+                               (r"/node/([^/]*)", NodeHandler),
+                               (r'/wbsckt/([^/]*)', WebSocket)],
+                              **app_settings)
+
+    application.listen(address=settings.ADDRESS, port=settings.PORT)
+    logger.info("Server started. Listen {host}:{port}.".format(host=settings.ADDRESS,
+                                                               port=settings.PORT))
+    # run reactor
+    IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ static/js/main.js
@@ -0,0 +1,121 @@
+var initiator;
+var pc;
+var ws = new WebSocket(location.href.replace('http', 'ws').replace('node', 'wbsckt'));
+var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+
+function socketCallback(event) {
+    if (event.data == "magic_overload") {
+        alert("Sorry, but this node is overloaded!");
+    }
+    if (event.data == "owner") {
+        initiator = false;
+        initialize();
+    }
+    if (event.data == "guest") {
+        initiator = true;
+        initialize();
+    }
+}
+// add handler
+ws.onmessage = socketCallback;
+
+function initialize() {
+    var constraints = {
+        audio: false,
+        video: true
+    };
+    navigator.getUserMedia(constraints, success, fail);
+}
+
+function success(stream) {
+    pc = new PeerConnection(null);
+
+    if (stream) {
+        pc.addStream(stream);
+        $('#local').attachStream(stream);
+    }
+
+    pc.onaddstream = function(event) {
+        $('#remote').attachStream(event.stream);
+        logStreaming(true);
+    };
+    pc.onicecandidate = function(event) {
+        if (event.candidate) {
+            ws.send(JSON.stringify(event.candidate));
+        }
+    };
+    ws.onmessage = function (event) {
+        var signal = JSON.parse(event.data);
+        if (signal.sdp) {
+            if (initiator) {
+                receiveAnswer(signal);
+            } else {
+                receiveOffer(signal);
+            }
+        } else if (signal.candidate) {
+            pc.addIceCandidate(new IceCandidate(signal));
+        }
+    };
+
+    if (initiator) {
+        createOffer();
+    } else {
+        log('Waiting for guest connection...');
+    }
+    logStreaming(false);
+}
+
+function fail() {
+    $('#traceback').text(Array.prototype.join.call(arguments, ' '));
+    $('#traceback').attr('class', 'bg-danger');
+    console.error.apply(console, arguments);
+}
+
+function createOffer() {
+    log('Creating offer. Please wait.');
+    pc.createOffer(function(offer) {
+        log('Success offer');
+        pc.setLocalDescription(offer, function() {
+            log('Sending to remote...');
+            ws.send(JSON.stringify(offer));
+        }, fail);
+    }, fail);
+}
+
+function receiveOffer(offer) {
+    log('Received offer.');
+    pc.setRemoteDescription(new SessionDescription(offer), function() {
+        log('Creating response');
+        pc.createAnswer(function(answer) {
+            log('Created response');
+            pc.setLocalDescription(answer, function() {
+                log('Sent response');
+                ws.send(JSON.stringify(answer));
+            }, fail);
+        }, fail);
+    }, fail);
+}
+
+function receiveAnswer(answer) {
+    log('received answer');
+    pc.setRemoteDescription(new SessionDescription(answer));
+}
+
+function log() {
+    $('#traceback').text(Array.prototype.join.call(arguments, ' '));
+    console.log.apply(console, arguments);
+}
+
+function logStreaming(streaming) {
+    $('#streaming').text(streaming ? '[streaming]' : '[..]');
+}
+
+jQuery.fn.attachStream = function(stream) {
+    this.each(function() {
+        this.src = URL.createObjectURL(stream);
+        this.play();
+    });
+};
